fc1ea4c961005e5ad6dccbe4a66a08b9f2055d6a
Jessica Scanlan <jessica.scanlan.1993@gmail.com>
2018-04-16 12:47:10 +1000

    Added attributes and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :bookings
+  has_many :plants
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  mount_uploader :photo, PhotoUploader
 end

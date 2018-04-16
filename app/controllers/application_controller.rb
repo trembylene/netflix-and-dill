0ef6f9f37a9eb58c474f2a96a29b0fefea3ceb6d
Daniel Kaga <daniel.kaga@gmail.com>
2018-04-16 16:22:08 +1000

    Added new user attributes to views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery
   before_action :authenticate_user!
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo, :photo_cache])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :last_name, :photo, :photo_cache])
   end
 end

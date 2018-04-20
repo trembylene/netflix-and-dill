0ca7157d24caab9592368b7c211b34fc81548341
Sergio Murakami <sergio_murakami@hotmail.com>
2018-04-20 16:12:38 +1000

    Fixed fixed merge

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -35,11 +35,7 @@ class PlantsController < ApplicationController
         lat: plant.latitude,
         lng: plant.longitude,
         id: plant.id,
-<<<<<<< HEAD
-        icon: '/assets/pin.png',
-=======
         icon: 'https://res.cloudinary.com/trembylene/image/upload/v1524193056/pin.png',
->>>>>>> cf489f6693d6be19dd46cd159957d525499f8513
         infoWindow: {
           content: "<a href='plants/#{plant.id}'><img src='#{plant.photo}' class='photo_markers' /><h5> $#{plant.cost}/day</h5><strong>#{plant.title}</strong><br>"
         }

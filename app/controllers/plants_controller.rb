660e8bc72b70f929a5976a8d5bdcd550e4e75c81
Daniel Kaga <daniel.kaga@gmail.com>
2018-04-20 16:24:30 +1000

    Fixed show page and map markers

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -37,7 +37,7 @@ class PlantsController < ApplicationController
         id: plant.id,
         icon: 'https://res.cloudinary.com/trembylene/image/upload/v1524193056/pin.png',
         infoWindow: {
-          content: "<a href='plants/#{plant.id}'><img src='#{plant.photo}' class='photo_markers' /><h5> $#{plant.cost}/day</h5><strong>#{plant.title}</strong><br>"
+          content: "<a href='#{plant.id}'><img src='#{plant.photo}' class='photo_markers' /><h5> $#{plant.cost}/day</h5><strong>#{plant.title}</strong><br>"
         }
       }
     end
@@ -69,7 +69,7 @@ class PlantsController < ApplicationController
       lng: @plant.longitude,
       icon: 'https://res.cloudinary.com/trembylene/image/upload/v1524193056/pin.png',
       infoWindow: {
-          content: "<a href='plants/#{plant.id}'><img src='#{plant.photo}' class='photo_markers' /><h5> $#{plant.cost}/day</h5><strong>#{plant.title}</strong><br>"
+          content: "<a href='#{@plant.id}'><img src='#{@plant.photo}' class='photo_markers' /><h5> $#{@plant.cost}/day</h5><strong>#{@plant.title}</strong><br>"
       }
     }]
   end
